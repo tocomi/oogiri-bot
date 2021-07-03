f0735ad58cc36e27b5fcd1d33f7a07580309ca8b
tocomi <tocomi@wish.ocn.ne.jp>
2021-07-03 16:39:25 +0900

    feat: setup api endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
 import * as functions from 'firebase-functions'
+import * as express from 'express'
+import * as cors from 'cors'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
-})
+const app = express()
+app.use(cors({ origin: true }))
+
+app.get('/odai/current', (req, res) => res.send({ status: 'OKOKOK' }))
+
+exports.api = functions.https.onRequest(app)
