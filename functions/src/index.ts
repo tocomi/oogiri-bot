897091df78c32674f2f4c3b48283681b3d47ba75
tocomi <tocomi@wish.ocn.ne.jp>
2021-07-03 16:48:59 +0900

    feat: change region

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,11 @@ import * as functions from 'firebase-functions'
 import * as express from 'express'
 import * as cors from 'cors'
 
+const REGION = 'asia-northeast1'
+
 const app = express()
 app.use(cors({ origin: true }))
 
 app.get('/odai/current', (req, res) => res.send({ status: 'OKOKOK' }))
 
-exports.api = functions.https.onRequest(app)
+exports.api = functions.region(REGION).https.onRequest(app)
