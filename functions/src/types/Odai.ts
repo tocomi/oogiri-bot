ba4d1ad8db2819f80c93da7d6efaf2e5426a1b13
tocomi <tocomi@wish.ocn.ne.jp>
2021-07-04 23:50:45 +0900

    refactor: update types

--- functions/src/types/Odai.ts
+++ functions/src/types/Odai.ts
@@ -2,25 +2,25 @@ import { ApiStatus, SlackParams } from './Api'
 
 export type OdaiApiStatus = 'duplication' | ApiStatus
 
-export type OdaiPostRequestParams = {
-  title: string
-  createdBy: string
-} & SlackParams
-
-type OdaiStatus = 'posting' | 'voting' | 'finished'
-
 type OdaiBase = {
   title: string
   createdBy: string
+}
+
+export type OdaiPostRequestParams = OdaiBase & SlackParams
+
+type OdaiStatus = 'posting' | 'voting' | 'finished'
+
+type OdaiApiBase = OdaiBase & {
   status: OdaiStatus
 }
 
-export type OdaiPostData = OdaiBase & {
+export type OdaiPostData = OdaiApiBase & {
   createdAt: Date
 }
 
 export type OdaiCurrentParams = SlackParams
 
-export type OdaiCurrentResponse = OdaiBase & {
+export type OdaiCurrentResponse = OdaiApiBase & {
   createdAt: number
 }
