1371f9f3d2e8c119a2cbba64ba9d73d1dbb80a8a
tocomi <tocomi@wish.ocn.ne.jp>
2021-07-13 23:27:59 +0900

    feat: make ranking method

--- src/kotae/makeRanking.ts
+++ src/kotae/makeRanking.ts
@@ -5,13 +5,26 @@ export const makeRanking = (kotaeList: Kotae[]): RankedKotae[] => {
     return a.votedCount > b.votedCount ? -1 : 1
   })
   let rank: RankedKotae['rank'] = 1
-  const rankedList = sortedList.map((kotae) => {
-    const ranked = {
-      ...kotae,
-      rank,
-    }
-    rank += 1
-    return ranked
-  })
+  let beforeCount = 0
+  let stockRank = 1
+  const rankedList = sortedList
+    .filter((kotae) => kotae.votedCount > 0)
+    .map((kotae) => {
+      if (beforeCount > 0) {
+        if (beforeCount === kotae.votedCount) {
+          stockRank += 1
+        } else {
+          rank += stockRank
+          stockRank = 1
+        }
+      }
+      beforeCount = kotae.votedCount
+      const ranked = {
+        ...kotae,
+        rank,
+      }
+      return ranked
+    })
+    .filter((kotae) => kotae.rank <= 3)
   return rankedList
 }
