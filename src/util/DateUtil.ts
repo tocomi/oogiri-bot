df1239e3583502ee4624f8a21ad3604188b10680
tocomi <tocomi0112@gmail.com>
2021-08-16 23:47:46 +0900

    fix: 期限の残り日数の計算ロジックを修正

--- src/util/DateUtil.ts
+++ src/util/DateUtil.ts
@@ -4,13 +4,21 @@ export const milliSecondsToYYYYMMDD = (milliSeconds: number | string) => {
   return dayjs(milliSeconds).format('YYYY/MM/DD')
 }
 
+/**
+ * 期限の日付と現在日付の日数差分から表示するメッセージを返す
+ * 現在は時間は設定できないので、endOfメソッドを使って設定された日付の一番遅い時間と比較している
+ * @param milliSeconds
+ * @returns string
+ */
 export const diffMessageFromCurrent = (milliSeconds: number) => {
-  const dateDiff = dayjs(milliSeconds).diff(dayjs(), 'day')
+  const dueDate = dayjs(milliSeconds).endOf('day')
+  const now = dayjs()
+
+  if (now.isAfter(dueDate)) return '過ぎてる！'
+
+  const dateDiff = dueDate.diff(dayjs(), 'day')
   if (dateDiff === 0) {
     return '今日まで！'
-  }
-  if (dateDiff < 0) {
-    return '過ぎてる！'
   }
   return `あと${dateDiff}日`
 }
