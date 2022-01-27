b0301ca47ff7195a20c5269b0e4aa74bddaa5ba8
tocomi <tocomi0112@gmail.com>
2022-01-27 20:42:53 +0900

    test: fix test

--- src/util/DateUtil.spec.ts
+++ src/util/DateUtil.spec.ts
@@ -6,24 +6,24 @@ describe('期限の差分日付計算', () => {
   test('期限より2日先', () => {
     const milliSeconds = new Date().getTime() + DAY * 2
     const result = diffMessageFromCurrent(milliSeconds)
-    expect(result).toBe('あと2日')
+    expect(result).toBe('あと2日！')
   })
 
   test('期限より1日先', () => {
     const milliSeconds = new Date().getTime() + DAY
     const result = diffMessageFromCurrent(milliSeconds)
-    expect(result).toBe('あと1日')
+    expect(result).toBe('あと1日！')
   })
 
   test('期限当日', () => {
     const milliSeconds = new Date().getTime()
     const result = diffMessageFromCurrent(milliSeconds)
-    expect(result).toBe('今日まで！')
+    expect(result).toBe('今日までですよー！')
   })
 
   test('期限翌日', () => {
     const milliSeconds = new Date().getTime() - DAY
     const result = diffMessageFromCurrent(milliSeconds)
-    expect(result).toBe('過ぎてる！')
+    expect(result).toBe('過ぎてます！')
   })
 })
