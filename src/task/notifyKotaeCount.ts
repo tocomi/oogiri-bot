c57b315be8c63786f3b97ed26911ce39c775fb23
Kenta TSUNEMI <25561744+tocomi@users.noreply.github.com>
2022-02-01 23:10:31 +0900

    #89 refactor: 回答と投票の状況確認をまとめる (#90)

--- src/task/notifyKotaeCount.ts
+++ src/task/notifyKotaeCount.ts
@@ -1,12 +1,12 @@
-import { App } from '@slack/bolt'
-import { config } from '../config'
+import { WebClient } from '@slack/web-api'
 import { countKotae } from '../kotae/action/countKotae'
 
-const app = new App({
-  // NOTE: ソケットモードである必要はないが、そうしないとエラーが出てしまう
-  socketMode: true,
-  token: config.slack.botToken,
-  appToken: config.slack.appToken,
-})
-
-countKotae({ slackTeamId: config.slack.teamId, client: app.client, isScheduler: true })
+export const notifyKotaeCount = async ({
+  slackTeamId,
+  client,
+}: {
+  slackTeamId: string
+  client: WebClient
+}) => {
+  await countKotae({ slackTeamId, client, isScheduler: true })
+}
