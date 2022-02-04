9ea6ac66bcbdc5f3614efa0184728863f4155871
tocomi <tocomi0112@gmail.com>
2022-02-04 20:18:31 +0900

    #81 feat: 投票開始時にルールを周知する

--- src/vote/blocks/createVoteStartBlocks.ts
+++ src/vote/blocks/createVoteStartBlocks.ts
@@ -27,7 +27,21 @@ export const createVoteStartBlocks = ({
       type: 'section',
       text: {
         type: 'mrkdwn',
-        text: '回答の右のボタンを押すと投票できます！ 投票は複数回できますよ！',
+        text: '回答の右のボタンを押すと投票できます！',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: ':point_right: :first_place_medal:*5ポイント* (1票のみ) :second_place_medal:*3ポイント* (1票のみ) :third_place_medal:*1ポイント* (無制限)',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: ':warning: 同一回答への複数投票はできません:woman-gesturing-no:',
       },
     },
     {
