72d287bf129dd6714fb3a6811ef433f8c96c199b
Kenta TSUNEMI <25561744+tocomi@users.noreply.github.com>
2022-04-14 21:29:26 +0900

    #98 feat: 投票の説明は最後に表示する (#99)
    * feat: use divider
    
    * feat: add footer

--- src/vote/blocks/createVoteStartHeaderBlocks.ts
+++ src/vote/blocks/createVoteStartHeaderBlocks.ts
@@ -1,6 +1,6 @@
 import { KnownBlock } from '@slack/types'
 
-export const createVoteStartBlocks = ({
+export const createVoteStartHeaderBlocks = ({
   title,
   imageUrl = '',
 }: {
@@ -27,27 +27,6 @@ export const createVoteStartBlocks = ({
       type: 'section',
       text: {
         type: 'mrkdwn',
-        text: '回答の右のボタンを押すと投票できます！',
-      },
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: ':point_right: :first_place_medal:*5P* (1票のみ) :second_place_medal:*3P* (1票のみ) :third_place_medal:*1P* (無制限)',
-      },
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: ':warning: 同一回答への複数投票はできません:woman-gesturing-no:',
-      },
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
         text: `:speech_balloon: *お題: ${title}*`,
       },
     }
@@ -60,11 +39,7 @@ export const createVoteStartBlocks = ({
     })
   }
   blocks.push({
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: '---',
-    },
+    type: 'divider',
   })
   return blocks
 }
